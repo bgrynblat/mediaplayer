53efbfd8df08bd4593a6891604906813e24df842
Benjamin Grynblat <benjamin.grynblat@gmail.com>
2016-06-01 09:31:38 +0000

    fix update files on local

--- update_files.php
+++ update_files.php
@@ -20,63 +20,7 @@ if($_ENV["server"]["host"] != "localhost" && $_ENV["server"]["host"] != "127.0.0
         HTTPRequest($file, $file);
 	
 } else {
-
-
-	$tmp_folder = $_ENV["TMP_FOLDER"];
-	$filename = "files.last";
-	$tmp_filename = "files.tmp";
-
-	$file = "$tmp_folder/$filename";
-	$tmp = "$tmp_folder/$tmp_filename";
-
-	$checks = 0;
-	while(file_exists($tmp) && $checks <= 10) {
-		$checks++;
-		if($checks >= 10) {
-			echo "Timeout !";
-			exit();
-		}
-	}
-
-	$now = time();
-
-	$update=false;
-	if(file_exists($file) && !$force) {
-		$filetime = filemtime($file);
-	
-		//echo "$file was last modified: ".$filetime;
-		//echo "<br/>";
-			//echo "Now: ".time();
-		
-		if($now > $filetime+60) {
-			unlink($file);
-			$update = true;
-		}
-	} else $update = true;
-
-	if($update) {
-		//echo "Lets do it";
-		$files = getFiles();
-
-		$handle = fopen($tmp, "w");
-		fwrite($handle, "{\"updated\":\"".($now+1000)."\",\"files\":[");
-	
-		//{url, full_path, filename}
-		for($i = 0; $i<count($files); $i++) {
-			$f = $files[$i];
-		
-			fwrite($handle, "{\"url\":\"$f[0]\",");
-			fwrite($handle, "\"path\":\"$f[1]\",");
-			fwrite($handle, "\"filename\":\"$f[2]\"}");
-			
-			if($i < count($files)-1)	fwrite($handle, ",");
-		}
-		
-		fwrite($handle, "]}");
-		fclose($handle);
-	
-		rename($tmp, $file);
-	}
+	executeCommand("scripts/update_files.sh > ".$file);
 }
 
 header('Content-Type: application/json');
